c2e8de0806d473c26e7b1138b4b1741bbaaaf2c8
diak <diak@mesterlevel.lo>
2026-03-12 13:27:51 +0100

    nyelv es api elkezdese

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -6,31 +6,35 @@ import { Link, useNavigate } from "react-router-dom";
 import csetliLogo from "../kepek/csetli.png";
 import Profilkep from "../kepek/search.webp";
 import "../style/style.css"
+import useLanguage from "../language";
 
 export default function HomePage() {
+    const lang = useLanguage("0")
 
     return (
-        <>
-            <div className="d-flex vh-100 justify-content-center align-items-center background">
-                <div >
+        <div className="background">
+
+            <div>
+                <div className="d-flex vh-100 justify-content-center align-items-center ">
+
                     <div>
-                        <img src={csetliLogo} alt="csetliLogo" width={400}/>
+                        <img src={csetliLogo} alt="csetliLogo" width={400} />
                     </div>
 
                     <div>
-                        <Link to="/registration" className=" text-decoration-none csetliColor">Regisztráció</Link>
+                        <Link to="/registration" className=" text-decoration-none csetliColor">{lang.registration}</Link>
                     </div>
 
                     <div>
-                        <Link to="/login" className=" text-decoration-none csetliColor ">Bejelentkezés</Link>
+                        <Link to="/login" className=" text-decoration-none csetliColor ">{lang.login}</Link>
                     </div>
 
                     <div>
-                        <Link to="/about" className=" text-decoration-none csetliColor ">Rólunk</Link>
+                        <Link to="/about" className=" text-decoration-none csetliColor ">{lang.abautus}</Link>
                     </div>
 
                 </div>
             </div>
-        </>
+        </div>
     )
 }        
